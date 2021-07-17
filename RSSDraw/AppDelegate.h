7a5911c5fdd883fbb80b1263d89f23adc35a30d2
Dmitriy Komar <dmitriyko@playtika.com>
2021-07-17 09:52:23 +0200

    Initial Commit

---
+++ RSSDraw/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  RSSDraw
+//
+//  Created by Dmitriy Komar on 7/17/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RSSDraw/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RSSDraw
+//
+//  Created by Dmitriy Komar on 7/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
